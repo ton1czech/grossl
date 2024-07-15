a54df64f99db548dc4bb5b7eb65c55a6365f8bfb
ton1czech <tonyasek007@gmail.com>
2024-07-15 15:40:17 +0200

    👌IMPROVE: homepage hero image, copyright, layout, spacing

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link'
 
 export default function Page() {
   return (
-    <main className='p-16 h-screen'>
-      <div className='flex justify-between text-lg'>
+    <main className='p-8 md:p-16 h-screen flex flex-col justify-between'>
+      <div className='flex flex-col md:gap-20 md:flex-row md:justify-between md:text-lg'>
         <div className='order-2 md:order-1'>
-          <h1 className='text-2xl font-semibold'>
+          <h1 className='text-lg md:text-2xl font-semibold my-10 md:mt-0 md:mb-6'>
             Dominik is a business minded professional lifestyle content creator,{' '}
-            <br />
+            <br className='hidden md:block' />
             specializing in social media and marketing.
           </h1>
-          <div className='mt-4 flex flex-col text-zinc-300'>
+          <div className='flex flex-col text-zinc-300'>
             <Link href='mailto:contact@dominikgrossl.com'>
               contact@dominikgrossl.com
             </Link>
@@ -18,7 +18,7 @@ export default function Page() {
           </div>
         </div>
 
-        <div className='order-1 md:order-2 flex flex-col text-zinc-300 text-right'>
+        <div className='order-1 md:order-2 flex flex-col text-zinc-300 md:text-right'>
           <Link href='/about'>About</Link>
           <Link href='https://www.instagram.com/dominik_grossl/'>
             Instagram
@@ -29,7 +29,13 @@ export default function Page() {
         </div>
       </div>
 
-      <h3 className='text-[28.8vw] font-bold text-center'>Grössl</h3>
+      <div>
+        <img src='/hero.png' alt='grössl' className='w-full' />
+        <p className='pt-5 md:pt-10 text-[8px] md:text-[10px] text-zinc-500 text-center'>
+          &copy; {new Date().getFullYear()} Dominik Grössl | ICO: 14184397 | 345
+          06, Němčice 26
+        </p>
+      </div>
     </main>
   )
 }
